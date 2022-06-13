b316034f7ebcfd67926662eaddbb26f007eb4885
caeberli <caeberli@mit.edu>
2022-06-13 14:15:02 -0400

    jsun

--- recommender_demo/scripts/testir.py
+++ recommender_demo/scripts/testir.py
@@ -2,11 +2,11 @@ import sys
 import json
 import ast
 
-data_to_pass_back = "blablabla"
+#data_to_pass_back = "blablabla"
 
-input = ast.literal_eval(sys.argv[1])
-output = input
-output["data_returned"] = data_to_pass_back
-print(json.dumps(output))
+#input = ast.literal_eval(sys.argv[1])
+#output = input
+#output["data_returned"] = data_to_pass_back
 
+print(json.dumps("blablabla"))
 sys.stdout.flush()
